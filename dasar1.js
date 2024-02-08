c271427477cf7392ed1401d0b669baa76955b38e
ZerdanZ <ardanaadinata23@gmail.com>
2024-02-08 18:12:57 +0700

    Ardana 2024 #BISA CODING

---
+++ dasar1.js
@@ -0,0 +1,74 @@
+//cara membuat variabel
+var a = ''
+let b = ''
+const c = ''
+
+// variabel bisa menampung string/text , integer/angka , boolen , array , object
+let nama = 'Ardana' // string
+let usia = 12 // integer
+let apakahBenar = true // boolen
+let uang = 5000.5 // integer juga namun ada koma
+
+// operator arimatika
+
+let angka1 = 10
+let angka2 = 20
+console.log(angka1 + angka2, 'Pertambahan');
+console.log(angka1 - angka2, 'Pengurangan');
+console.log(angka1 * angka2, 'Perkalian');
+console.log(angka1 / angka2, 'Pembagian');
+console.log(angka1 % angka2, 'Entah');
+
+
+// disini kita belajar mengisi variaberl
+
+let penampunganHasilOperatorAritmatika = angka1 + angka2;
+console.log(penampunganHasilOperatorAritmatika)
+
+penampunganHasilOperatorAritmatika = angka1 - angka2;
+console.log(penampunganHasilOperatorAritmatika)
+
+penampunganHasilOperatorAritmatika = angka1 * angka2;
+console.log(penampunganHasilOperatorAritmatika)
+
+penampunganHasilOperatorAritmatika = angka1 / angka2;
+console.log(penampunganHasilOperatorAritmatika)
+
+penampunganHasilOperatorAritmatika = angka1 % angka2;
+console.log(penampunganHasilOperatorAritmatika)
+
+// String bisa di tambah namun string tidak bisa dikurang dalam aritmatika
+
+let firstName = 'Ardana'
+let lastName = 'Adinata'
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+console.log(firstName - lastName) // NaN
+
+// Pembacaan code
+// Untuk pembacaan code harus di baca dari kiri ke kanan atau dari atas ke bawah
+// contoh pembacaan code
+
+let number = 20
+console.log(number + 10) // 30
+number = 10
+console.log(number + 10) // 20
+number = false
+console.log(number + 10) // 10
+number = true
+console.log(number + 10) // 11
+
+// Pertanyaan , Kenapa False + 10 = 10??? Karena False bernilai 0 , sedangkan true bernilai 1 , Maka True + 10 = 11
+
+// Membaca Error
+// Error jangan kalian bikin takut , Yang harus kalian takuti adalah jika tidak ada error dalam pemograman nya
+// Contoh Error
+
+// const hewan = "Jerapah"
+// console.log(hewan)
+// hewan = "Gajah"
+
+// Untuk komentar bisa tekan ctrl + /
+
+// Tips dalam coding adalah teliti , Tetap semangat Coding untuk masa depan yang indah #ArdanaAdinata - 08/02/2024
+// Coding adalah Habbit jadi selalu membiasakan lah diri dalam ber coding
